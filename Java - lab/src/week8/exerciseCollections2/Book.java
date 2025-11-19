82de6bac08e5a81fe09c83aa6d7b54ae1ac57f96
Yoanna <yoanna.yordanova2402@gmail.com>
2025-11-19 18:12:13 +0200

    Adding exercises with collections using overrithing methods

--- Java - lab/src/week8/exerciseCollections2/Book.java
+++ Java - lab/src/week8/exerciseCollections2/Book.java
@@ -1,4 +1,5 @@
 package week8.exerciseCollections2;
+import java.util.Objects;
 
 public class Book {
     public String title;
@@ -25,10 +26,24 @@ public class Book {
     public void setAuthor(String  author){
         this.author = author;
     }
-    public int getYear(){
-        return year;
-    }
+    public int getYear(){return year;}
     public void setAge(int year){
         this.year = year;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(title,author);
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (!(obj instanceof Book other)) return false;
+
+        return Objects.equals(title, other.title) && Objects.equals(author, other.author);
+    }
+    @Override
+    public String toString(){
+        return "Title: " + title + " Author: " + author + " Year: " + year;
     }
 }
--- Java - lab/src/week8/exerciseCollections2/Main.java
+++ Java - lab/src/week8/exerciseCollections2/Main.java
@@ -2,11 +2,41 @@ package week8.exerciseCollections2;
 import java.util.*;
 
 public class Main {
-//    public static void main(String[] args){
-//        Scanner input = new Scanner(System.in);
-//        @Override
-//        int hashCode(){
-//
-//        }
-//    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        Set<Book> booksHashSet = null;
+        while(n>0){
+            try {
+                System.out.println("Title: ");
+                String title = input.nextLine();
+                System.out.println("Author: ");
+                String author = input.nextLine();
+                System.out.println("Released year: ");
+                int year = Integer.parseInt(input.nextLine());
+                n--;
+
+                Book book = new Book(title,author,year);
+                booksHashSet = new HashSet<Book>();
+                booksHashSet.add(book);
+            }catch(NumberFormatException e){
+                System.out.println("Format incorrect!");
+            }catch(Exception e){
+                e.getMessage();
+            }
+        }
+        Map<String,Book> booksHashMap = new HashMap<String,Book>();
+        for(Book book: booksHashSet){
+            booksHashMap.put(book.getTitle(),book);
+        }
+        System.out.println("Enter title: ");
+        String titleResult = input.nextLine();
+
+        if(booksHashMap.containsKey(titleResult)) {
+            System.out.println("Book found: " + booksHashMap.get(titleResult).toString());
+        }
+        else{
+            System.out.println("Book not found!");
+        }
+    }
 }
